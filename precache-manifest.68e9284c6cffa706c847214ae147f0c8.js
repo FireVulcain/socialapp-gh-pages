2bfd1f95ade6df0c2f972d261a1dfba8443ddd7d
FireVulcain <n.disant@hotmail.com>
2021-02-11 14:13:51 +0100

    Updates

---
+++ precache-manifest.68e9284c6cffa706c847214ae147f0c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242eb7ef0cf1a71c2b007a3d86b3e3ed",
+    "url": "/socialapp-gh-pages/index.html"
+  },
+  {
+    "revision": "7d69fff6fb266ea12c38",
+    "url": "/socialapp-gh-pages/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "446ae14099a7178d3837",
+    "url": "/socialapp-gh-pages/static/css/main.8ac6fdfe.chunk.css"
+  },
+  {
+    "revision": "7d69fff6fb266ea12c38",
+    "url": "/socialapp-gh-pages/static/js/2.fc04c219.chunk.js"
+  },
+  {
+    "revision": "3cfe650a1379d85f51205df5c3fe83a0",
+    "url": "/socialapp-gh-pages/static/js/2.fc04c219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446ae14099a7178d3837",
+    "url": "/socialapp-gh-pages/static/js/main.43c97dac.chunk.js"
+  },
+  {
+    "revision": "f5a69a8b8693bfa73c4f",
+    "url": "/socialapp-gh-pages/static/js/runtime-main.6dc00c5c.js"
+  }
+]);
